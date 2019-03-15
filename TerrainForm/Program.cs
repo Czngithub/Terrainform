cd6f0ab2e150c235f6be76d55beb37dd3f020a98
Czngithub <48195153+Czngithub@users.noreply.github.com>
2019-03-15 10:11:23 +0800

    TerrainForm

---
+++ TerrainForm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerrainForm
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Form1 form = new Form1();
+            Boolean b = form.InitializeDirect3D();
+            if (b==false)
+            {
+                MessageBox.Show("无法启动Direct3D","错误");
+                return;
+            }
+            form.Show();//如果初始化成功，则显示窗体
+            while (form.Created)
+            {
+                form.Render();
+                Application.DoEvents();
+            }
+            //form.Dispose();
+        }
+    }
+}
